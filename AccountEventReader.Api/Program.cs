578a405a5222431fcf1ac2844661096f143aed7a
Nic Schmitz <traceff@hotmail.com>
2022-11-01 20:45:43 -0600

    async all the things! and clean up

--- AccountEventReader.Api/Program.cs
+++ AccountEventReader.Api/Program.cs
@@ -7,12 +7,13 @@ using System.Text.Json;
 
 internal class Program
 {
-    public static MessageMapperFactory _messageMapperFactory;
-    public static IMessageProducer _secondaryProducer;
-    public static ISession session;
-    public static JsonSerializerOptions defaultOptions = new JsonSerializerOptions { WriteIndented = false, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
+    private static MessageMapperFactory _messageMapperFactory;
+    private static IMessageProducer _secondaryProducer;
+    private static ISession _session;
+    private static IConnection _connection;
+    private static JsonSerializerOptions _defaultSerializerOptions = new JsonSerializerOptions { WriteIndented = false, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
 
-    private static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         _messageMapperFactory = new MessageMapperFactory();
 
@@ -21,56 +22,57 @@ internal class Program
         var cisMessageFaker = new Faker<CisMessage>()
             .RuleFor(x => x.EventType, x => x.PickRandom(new List<string>() { "NEWBILL", "BUDSTOP", "BUDSTOPCC" }))
             .RuleFor(x => x.AccountId, x => x.Random.Long())
-            .RuleFor(x => x.NotificationEventId, x => x.Random.Long().ToString());
-
-        var options = new JsonSerializerOptions { WriteIndented = false, DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
+            .RuleFor(x => x.NotificationEventId, x => x.Random.Long().ToString());        
 
         var brokerUri = new Uri("activemq:tcp://192.168.1.208:61616");
         var factory = new ConnectionFactory(brokerUri);
 
-        var connection = factory.CreateConnection();
-        connection.Start();
+        _connection = await factory.CreateConnectionAsync();
+        await _connection.StartAsync();
+        
+        _session = await _connection.CreateSessionAsync(Apache.NMS.AcknowledgementMode.AutoAcknowledge);
+        
+        var destination = await _session.GetQueueAsync("testqueue");
+        var secondaryDestination = await _session.GetQueueAsync("testoutput");
 
-        session = connection.CreateSession(Apache.NMS.AcknowledgementMode.AutoAcknowledge);
-
-        var destination = session.GetQueue("testqueue");
-
-        var secondaryDestination = session.GetQueue("testoutput");
-
-        var producer = session.CreateProducer(destination);
+        var producer = await _session.CreateProducerAsync(destination);        
         producer.DeliveryMode = Apache.NMS.MsgDeliveryMode.Persistent;
 
-        var consumer = session.CreateConsumer(destination);
+        var consumer = await _session.CreateConsumerAsync(destination);
         consumer.Listener += Consumer_Listener;
 
-        _secondaryProducer = session.CreateProducer(secondaryDestination);
+        _secondaryProducer = await _session.CreateProducerAsync(secondaryDestination);
         _secondaryProducer.DeliveryMode = MsgDeliveryMode.Persistent;
-
-        while (true)
+      
+        Console.WriteLine("Press ESC to stop");
+        do
         {
-            var cisMessage = cisMessageFaker.Generate();
-
-            var serializedThing = JsonSerializer.Serialize(cisMessage, options);
-            producer.Send(session.CreateTextMessage(serializedThing));
-        }
-
-        session.Close();
-        connection.Close();
+            while (!Console.KeyAvailable)
+            {
+                var cisMessage = cisMessageFaker.Generate();
+                var serializedCisMessage = JsonSerializer.Serialize(cisMessage, _defaultSerializerOptions);
+                var message = await _session.CreateTextMessageAsync(serializedCisMessage);
+                await producer.SendAsync(message);                
+            }
+        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                
+        await _session.CloseAsync();
+        await _connection.CloseAsync();
     }
-
-    private static void Consumer_Listener(Apache.NMS.IMessage message)
+  
+    private static async void Consumer_Listener(Apache.NMS.IMessage message)
     {
         var txtMessage = message as ITextMessage;
         var thing = JsonSerializer.Deserialize<CisMessage>(txtMessage.Text);
 
-        Console.WriteLine($"{thing.EventType}");
+        Console.WriteLine($"Received event type: {thing.EventType}");
         var mapper = _messageMapperFactory.Create(thing.EventType);
 
         var mappedThing = mapper.Map(thing);
-        Console.WriteLine($"{mappedThing.EventType}");
+        Console.WriteLine($"Mapped to thing: {mappedThing.EventType}");
 
-        var serializedMappedThing = JsonSerializer.Serialize(mappedThing, defaultOptions);
-
-        _secondaryProducer.Send(session.CreateTextMessage(serializedMappedThing));
+        var serializedMappedThing = JsonSerializer.Serialize(mappedThing, _defaultSerializerOptions);
+        var outboundMessage = await _session.CreateTextMessageAsync(serializedMappedThing);
+        await _secondaryProducer.SendAsync(outboundMessage);
     }
 }
